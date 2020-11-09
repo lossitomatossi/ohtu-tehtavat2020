b8bffd287ded815af092e917a4811ee0736e653f
lossitomatossi <tomas.terala@gmail.com>
2020-11-09 20:34:38 +0200

    pääohjelma springil

--- viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -1,7 +1,6 @@
 package ohtu;
 
 import ohtu.verkkokauppa.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -10,14 +9,9 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        Viitegeneraattori viitegen = ctx.getBean(Viitegeneraattori.class);
         Kirjanpito kirjanpito = ctx.getBean(Kirjanpito.class);
-        Varasto varasto = ctx.getBean(Varasto.class);
-        Pankki pankki = ctx.getBean(Pankki.class);
-        Kauppa kauppa = new Kauppa(varasto,
-                pankki,
-                viitegen);
-
+        Kauppa kauppa = ctx.getBean(Kauppa.class);
+    
         // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
         kauppa.aloitaAsiointi();
         kauppa.lisaaKoriin(1);
