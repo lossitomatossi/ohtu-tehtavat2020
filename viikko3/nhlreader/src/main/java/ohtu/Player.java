ab845379791beef6256ccdcc0879f9822ada9d84
lossitomatossi <tomas.terala@gmail.com>
2020-11-16 21:54:37 +0200

    viikko3t1 valmis

---
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -0,0 +1,74 @@
+package ohtu;
+
+public class Player {
+
+    private String name;
+    private String nationality;
+    private int assists;
+    private int goals;
+    private int penalties;
+    private String team;
+    private int games;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setPenalties(int penalties) {
+        this.penalties = penalties;
+    }
+
+    public int getPenalties() {
+        return penalties;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setGames(int games) {
+        this.games = games;
+    }
+
+    public int getGames() {
+        return games;
+    }
+
+    @Override
+    public String toString() {
+        return name + " team " + team + " goals " + goals + " assists " + assists;
+    }
+
+}
